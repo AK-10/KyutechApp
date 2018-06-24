46d0eb8f6e8e61fae7f05166bbbfcae79d7ed1e9
Atsushi KONISHI <crossover.ak@outlook.jp>
2018-06-24 17:45:37 +0900

    rm git cache

--- Pods/Target Support Files/Pods-KyutechApp/Pods-KyutechApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KyutechApp : NSObject
-@end
-@implementation PodsDummy_Pods_KyutechApp
-@end
--- Pods/Target Support Files/Pods-KyutechAppUITests/Pods-KyutechAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KyutechAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KyutechAppUITests
-@end
